5db2ec531ea8a6a1a881b1d4598f7a8242030a3e
JmacGhost <39404820+JmacGhost@users.noreply.github.com>
2020-10-13 20:57:23 +0100

    Update jermaine-truelayer.sh

--- jermaine-truelayer.sh
+++ jermaine-truelayer.sh
@@ -4,14 +4,17 @@ TESTFILE=TLTest.txt
 
 
 function tltest {
+	# Check for directory for if Truelayer exists and if truelayer doesn't exists then make directory.
 	if [ -d "./Truelayer" ]
 	then 
 	echo "Directory Truelayer exists."
 	else 
 	mkdir -p Truelayer
 	fi 
+	# Creates test file (TLTest.txt)
 	cd Truelayer; touch $TESTFILE
 	CWD="$(pwd)"
+	# Get time,file,IP,Memory,Process ID and User name.
 
 	TIME=`date '+%Y-%m-%d %H:%M:%S'`
 	FILE=`find . -name '*' | sed 's/\.\///g'`
@@ -20,8 +23,9 @@ function tltest {
 	PID=`echo $$`
 	WHOAMI=`whoami`
 
-	   
+	# Redirect all outputs to test file.   
 	echo -e ${TIME}'\t'${FILE}'\t'${IPADDR}'\t'${MEMORY}'\t'${PID}'\t'${WHOAMI}'\n' >> $CWD/$TESTFILE
+	# Redirect file list to test file.
 
 	ls -la $CWD >> $CWD/$TESTFILE
 
@@ -29,5 +33,5 @@ function tltest {
 }
 
 
-
+# Call main function.
 tltest
